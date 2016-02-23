fc115b5f681877a357e3015ee2662374ca0d5790
Robert O'Connor <rob@oconnor.ninja>
2016-02-22 19:43:11 -0500

    Temporary fix

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ catch (e) {
 }
 
 if (schema) {
-    console.log(`babel-relay-plugin-loader: using schema at [${schemaPath}]`);
+    // console.log(`babel-relay-plugin-loader: using schema at [${schemaPath}]`);
     module.exports = getbabelRelayPlugin(schema.data);
 } else {
     console.log(`babel-relay-plugin-loader: no schema found at [${schemaPath}]`);
